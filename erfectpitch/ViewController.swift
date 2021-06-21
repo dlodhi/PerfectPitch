02aa757e052791e1f7d7e9d43aef4f3ff271a95a
Deepak Lodhi <dlodhi123456@gmail.com>
2021-06-21 22:30:22 +0530

    recordAudio was updated
    recordAudio was updated

--- erfectpitch/ViewController.swift
+++ erfectpitch/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Recording_label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record Button was clicked")
+    }
+    
 }
 
