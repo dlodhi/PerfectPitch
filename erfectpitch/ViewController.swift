4de283bbf94f2945b6734181119a5a8a6e83abf9
Deepak Lodhi <dlodhi123456@gmail.com>
2021-06-23 14:28:51 +0530

    Update ViewController.swift
    viewWillAppear added

--- erfectpitch/ViewController.swift
+++ erfectpitch/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear is called ")
+    }
+    
 
     @IBAction func recordAudio(_ sender: Any) {
         print("Record Button was clicked")
